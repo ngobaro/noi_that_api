357b39168b30681c2b520a03e858a5c26bed4ae5
ngobaro <ngobaocoder@gmail.com>
2026-04-04 11:44:37 +0700

    create project

---
+++ src/main/java/com/baro/noi_that_api/NoiThatApiApplication.java
@@ -0,0 +1,13 @@
+package com.baro.noi_that_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoiThatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoiThatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baro/noi_that_api/NoiThatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baro.noi_that_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoiThatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
